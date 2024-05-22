2f9c119218c2cb172c004bae86cb1123c149b25d
Erick Kramer <erickkramer@gmail>
2024-05-22 12:03:10 +0200

    ref: Clean up root

--- cmd/root.go
+++ cmd/root.go
@@ -15,10 +15,6 @@ var rootCmd = &cobra.Command{
 	Use:   "rv",
 	Short: "Fast CLI tool for managing multiple Git repositories.",
 	Long:  ``,
-
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
