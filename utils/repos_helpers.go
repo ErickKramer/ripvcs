6fad86b8805b34db6485d1019fb2ec5ff7965a76
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2025-05-22 10:35:09 +0900

    Resolve exclude correctly

--- utils/repos_helpers.go
+++ utils/repos_helpers.go
@@ -108,13 +108,29 @@ func ParseReposFile(filePath string) (*Config, error) {
 }
 
 // FindReposFiles Search .repos files in a given path
-func FindReposFiles(rootPath string) ([]string, error) {
+func FindReposFiles(rootPath string, clonedPaths []string) ([]string, error) {
 	var foundReposFiles []string
 	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && filepath.Ext(path) == ".repos" {
-			if err != nil {
-				return err
+		if err != nil {
+			return err
+		}
+		// If clonedPaths is not empty, only consider files under those directories
+		if len(clonedPaths) > 0 {
+			matched := false
+			for _, clonedPath := range clonedPaths {
+				absClonedPath, _ := filepath.Abs(clonedPath)
+				absPath, _ := filepath.Abs(path)
+				rel, relErr := filepath.Rel(absClonedPath, absPath)
+				if relErr == nil && (rel == "." || !strings.HasPrefix(rel, "..")) {
+					matched = true
+					break
+				}
 			}
+			if !matched {
+				return nil
+			}
+		}
+		if !info.IsDir() && (filepath.Ext(path) == ".repos") {
 			foundReposFiles = append(foundReposFiles, path)
 		}
 		return nil
